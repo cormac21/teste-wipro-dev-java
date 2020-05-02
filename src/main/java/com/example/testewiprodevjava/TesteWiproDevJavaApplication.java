708c416ef8f79c7f10558945461bd541a3053268
Lucas Gaio <gaio.lucas0@gmail.com>
2020-05-02 11:07:49 -0300

    Direto do forno

---
+++ src/main/java/com/example/testewiprodevjava/TesteWiproDevJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testewiprodevjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteWiproDevJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteWiproDevJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testewiprodevjava/TesteWiproDevJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testewiprodevjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteWiproDevJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
